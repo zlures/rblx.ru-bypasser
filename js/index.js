290b06e89ca22514b86df69e0988a0c8fda1f42a
Vlada <81084648+zlures@users.noreply.github.com>
2022-01-03 11:34:36 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(function() {
+	var currency = {
+		coefficient: 2
+	};
+
+	$("input[name='amount']").keyup(function() {
+		$("b#amount").html((this.value * currency.coefficient).toFixed(1) + " СЂСѓР±.");
+	});
+
+	$("input[name='amount']").bind("change keyup input click", function() {
+	    if (this.value.match(/[^0-9]/g)) {
+	        this.value = this.value.replace(/[^0-9]/g, "");
+	    }
+	});
+
+});
